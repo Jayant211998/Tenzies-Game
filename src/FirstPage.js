e76ba6aee2df70f1c83205eb8fdda3d614d63507
Jayant211998 <jayantgawali98@gmail.com>
2022-10-25 15:36:53 +0530

    new changes

--- src/FirstPage.js
+++ src/FirstPage.js
@@ -1,7 +1,7 @@
 export default function FirstPage(props){
     return(
         <div className="fisrtpage">
-        <h1>Quizzica!</h1>
+        <h1>Quiz App</h1>
         <p>Test Your Skills</p>
         <button onClick={props.controlStart} className="start-button">Start Quiz</button>
         </div>
--- src/Quiz.js
+++ src/Quiz.js
@@ -5,15 +5,16 @@ let score = 0
 export default function Quiz(props){
     const [questionData,setQuestionData] = React.useState([])
     const [isSubmit,setIsSubmit] = React.useState(false)
-    const [ansShown,setAnsShown] =React.useState(false)
-    const [disabled,setDisabled] =React.useState(false)
-    
+    const [ansShown,setAnsShown] = React.useState(false)
+    const [disabled,setDisabled] = React.useState(false)
+    const [submitButton,setSubmitButton] = React.useState(false);
 
 React.useEffect(async() =>{
-         const res = await fetch("https://opentdb.com/api.php?amount=5")
-         const json=await res.json()
-         const data = json.results;
-         setQuestionData(prev=>{
+    async function fetchQuestions(){
+        const res = await fetch("https://opentdb.com/api.php?amount=5")
+        const json=await res.json();
+        const data = json.results;
+        setQuestionData(prev=>{
              for(let i=0;i<5;i++){
                 data[i]={...data[i],
                     id:i,
@@ -29,10 +30,12 @@ React.useEffect(async() =>{
                 }
                 data[i].allOptions = data[i].allOptions.map(opt=>{return {value: opt,btnColor:"grey"}})
              }
-             return data
-         })
-         },[] )
-// console.log(questionData)
+             return data;
+        })
+        setSubmitButton(true);
+        }
+        fetchQuestions();
+        },[] )
 
 
     function checkAnswer(){
@@ -86,22 +89,17 @@ React.useEffect(async() =>{
         });
         return (
             <div key={Math.floor(Math.random()*10000)}>
-            <p>{quest.question.replace(/&quot;/g, '\"')}</p>
+            <p>{quest.question.replace(/&quot;/g, '\"').replace(/&#039;/g, "\'")}</p>
             {optionList}
             <br/><hr/>
             </div>
         );
     })
-    
-
-    
-    
-    
     return(
         <div>
         {!isSubmit && <h1 className="quiz">Quiz</h1>}
-        {!isSubmit && questionsList}
-        {!isSubmit && !ansShown && <button onClick={checkAnswer} className="submit-button">Submit Test</button>}
+        {submitButton && !isSubmit && questionsList}
+        {submitButton && !isSubmit && !ansShown && <button onClick={checkAnswer} className="submit-button">Submit Test</button>}
         {!isSubmit && ansShown && <button onClick={submit} className="submit-button">Get Score</button>}
          {isSubmit && <div className="submit-page" > 
              <h1>Thanks For Giving Test</h1>
